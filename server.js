81786ec492fc92a92d86b552312d270ecdce7f3d
Houston Davis <hdavis.147@gmail.com>
2023-08-17 22:57:42 -0500

    Added comments to server.js

--- server.js
+++ server.js
@@ -9,8 +9,10 @@ const api = require('./routes/index');
 // Sets port to required value for Heroku, or 3001
 const PORT = process.env.PORT || 3001;
 
+// Creates a new instance of Express
 const app = express();
 
+// Express middleware to handle json
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -18,18 +20,22 @@ app.use(express.static('public'));
 
 app.use('/api', api);
 
+// No dir specified sends to index.html
 app.get('/', (req, res) => 
 res.sendFile(path.join(__dirname, '/Develop/public/index.html'))
 );
 
+// Unspecified dirs send to index.html
 app.get('*', (req, res) => 
 res.sendFile(path.join(__dirname, '/Develop/public/index.html'))
 );
 
+// GET route to notes.html
 app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/Develop/public/notes.html'))
 );
 
+// Express goes live, listening at specified port
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT}`)
 );
